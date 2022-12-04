1ab835b2dad996fa0acacceb64fe764b037bdca1
sh0116 <seokhyeon116@naver.com>
2022-12-04 07:08:26 -0800

    update

--- docker-images/count/count.sh
+++ docker-images/count/count.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-trap "exit" SIGINT
+
 mkdir /var/htdocs
-
 SET=$(seq 0 99999)
-
 for i in $SET
-
-do 
-    echo "Running loop seq "$i > /var/htdocs/index.html
-    sleep 10
+do
+    random_num="$(($RANDOM%1000))"
+    echo "Hello World?" $random_num > /var/htdocs/index.html
+    sleep 5
 done
